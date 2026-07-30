cfd0a281dd2cc27394283b5621e29ca311aa27c2
Anssi Nyman <anssi.nyman@pp1.inet.fi>
2026-07-30 19:26:54 +0300

    Add new timeout values also to sync redis client.

--- sync_redis_queue.py
+++ sync_redis_queue.py
@@ -1,4 +1,7 @@
 import redis
+import socket
+from redis.retry import Retry
+from redis.backoff import ExponentialBackoff
 from config import settings
 from logger import logger
 from custom_retry import retry
@@ -12,7 +15,23 @@ def get_redis() -> redis.Redis:
                     port=settings.redis_port,
                     db=settings.redis_db_number,
                     password=settings.redis_password.get_secret_value(),
-                    decode_responses=True
+                    decode_responses=True,
+
+                    socket_timeout=45,           # Must be higher than brpop timeout
+                    socket_connect_timeout=10,
+                    socket_keepalive=True,
+                    socket_keepalive_options={
+                        socket.TCP_KEEPIDLE: 60,
+                        socket.TCP_KEEPINTVL: 10,
+                        socket.TCP_KEEPCNT: 6,
+                    },
+                    health_check_interval=30,    # Very important
+                    retry_on_timeout=True,
+                    retry=Retry(
+                        ExponentialBackoff(base=0.1, cap=2.0),
+                        retries=5,
+                        supported_errors=(redis.exceptions.TimeoutError, redis.exceptions.ConnectionError)
+                    )
     )          
 
     try:
